435ce2cbd9c25fd5c580a33ad219cc7e89cc1e15
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-11-12 01:28:47 -0800

    adding assets

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import rstudio from "./tech/rstudio.png";
+import netframework from "./tech/netframework.png";
+import mvc from "./tech/mvc.png";
+import csharp from "./tech/csharp.png";
+import cplus from "./tech/cplus.png";
+import angularjs from "./tech/angularjs.png";
+import restapi from "./tech/restapi.png";
+
+import nss from "./company/nss.jpeg";
+import parktown from "./company/parktown.png";
+import turningpoint from "./company/turningpoint.jpeg";
+
+import authentic from "./authentic.png"
+import mailing from "./mailing.png"
+import booking from "./booking.png"
+import payment from "./payment.png"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  rstudio,
+  netframework,
+  mvc,
+  csharp,
+  cplus,
+  angularjs,
+  restapi,
+  nss,
+  parktown,
+  turningpoint,
+  authentic,
+  mailing,
+  booking,
+  payment
+};
